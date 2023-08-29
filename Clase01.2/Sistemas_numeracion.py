413cb52e5879b312bc151d661bc47de197208445
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-08-28 22:41:59 -0300

    Add_class01.2_part1

---
+++ Clase01.2/Sistemas_numeracion.py
@@ -0,0 +1,16 @@
+# Profundizando en los sistemas de numeracion
+
+# Sistema decimal
+a = 10
+
+# Sistema binario
+a =0b1010
+print(f'BINARIO: {a}')
+
+# Sistema octal
+a = 0o12
+print(f'OCTAL: {a}')
+
+# Sistema hexadecimal
+a = 0xA
+print(f'HEXADECIMAL: {a}')
