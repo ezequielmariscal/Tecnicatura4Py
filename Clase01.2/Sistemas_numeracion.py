a33e99519f41b36be8f08788fc67a50ad701b900
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-08-30 23:45:28 -0300

    Add_class2_part2

--- Clase01.2/Sistemas_numeracion.py
+++ Clase01.2/Sistemas_numeracion.py
@@ -2,6 +2,7 @@
 
 # Sistema decimal
 a = 10
+print(f'DECIMAL: {a}')
 
 # Sistema binario
 a =0b1010
@@ -14,3 +15,11 @@ print(f'OCTAL: {a}')
 # Sistema hexadecimal
 a = 0xA
 print(f'HEXADECIMAL: {a}')
+
+# Base Decimal
+a = int('23', 10)
+print(f'a = base decimal: {a}')
+
+#Base Binario
+a = int('10111', 2)
+print(f'a = base binario: {a}')
