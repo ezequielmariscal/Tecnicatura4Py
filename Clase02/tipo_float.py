e03a621d5a19c3e6c0842fddeb6d55982e79965d
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-08-31 21:51:04 -0300

    Add_class2_part5

--- Clase02/tipo_float.py
+++ Clase02/tipo_float.py
@@ -4,8 +4,17 @@ a = 3.0
 # Constructor de tipo floar -> puede recibir int y str
 a = float(10) # Le pasamos un tipo entero (int)
 a = float('10')
-print(f'a: {a:2.f}')
+print(f'a: {a:.2f}')
 
 # Notacion exponencial (valores positivos o negativos)
 a = 3e5
-print(f'a: {a}')
+print(f'a: {a:.2f}')
+
+a = 3e-5
+print(f'a: {a:.5f}')
+
+# Cualquier calculo que incluye un float, todo cambia a float
+
+a = 4.0 + 5
+print(a)
+print(type(a))
