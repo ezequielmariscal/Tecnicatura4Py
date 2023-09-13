a144df2813417edecc8cf35ebfb39388e8664865
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-09-12 23:52:14 -0300

    Add_class3_part4

--- Clase02/tipo_bool.py
+++ Clase02/tipo_bool.py
@@ -18,3 +18,33 @@ valor = 'Hola'
 resultado = bool(valor)
 print(f'valor {valor}, resultado: {resultado}')
 
+# Tipo colecciones -> False para colecciones vacias, True las demas
+
+# Lista (Array)
+valor = []
+resultado = bool(valor)
+print(f'valor de una lista vacia {valor}, resultado: {resultado}')
+
+valor = [2, 3, 4]
+resultado = bool(valor)
+print(f'valor lista con elementos{valor}, resultado: {resultado}')
+
+# Tuplas
+valor = ()
+resultado = bool(valor)
+print(f'valor de una tupla vacia {valor}, resultado: {resultado}')
+
+valor = (7,)
+resultado = bool(valor)
+print(f'valor de una tupla con elementos {valor}, resultado: {resultado}')
+
+# Diccionario
+valor = {}
+resultado = bool(valor)
+print(f'valor de un diccionario vacio {valor}, resultado: {resultado}')
+
+valor = {'Nombre': 'Pepe', 'Apellido': 'Argento'}
+resultado = bool(valor)
+print(f'valor de un diccionario con elementos {valor}, resultado: {resultado}')
+
+
