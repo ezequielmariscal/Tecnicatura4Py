91c1711b5cfd9fb215a75167d1c800358670d34b
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-09-12 23:39:26 -0300

    Add_class3_part2

---
+++ Clase02/tipo_bool.py
@@ -0,0 +1,10 @@
+# Bool contiene los valores True y False
+# Los tipos numericos, es false para el 0 (cero), true para los demas valores
+valor = 0.0
+resultado = bool(valor)
+print(f'valor {valor}, resultado: {resultado}')
+
+valor = 0.1
+resultado = bool(valor)
+print(f'valor {valor}, resultado: {resultado}')
+
