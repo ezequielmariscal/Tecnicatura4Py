a64316365b2d6f7a1f46dff600c981a270b5a80e
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-09-27 23:58:40 -0300

    Add_class5_part1.2

--- Clase02/mi_clase.py
+++ Clase02/mi_clase.py
@@ -1,5 +1,20 @@
 
 class MiClase:
     '''
-    Este es un eje
+    Este es un ejemplo de la documetacion de esta clase.
+    es solo para saber que aqui esta mi clase
     '''
+    def __init__(self):
+        '''
+        Aquui esta el metodo dunder init
+        Este metodo es el inicio
+        para esta clase
+        '''
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el metodo creado por mi que recibe parametrps
+        Detecto los parametros automaticamente
+        :param param1: Este es el parametro 1
+        :param param2: Este es el parametro 2
+        :return: Este es el valor de retorno
+        '''
