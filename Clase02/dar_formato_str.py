c2d74d274f15b9bc30b31e184771533a1a9dbfcb
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-10-02 23:05:32 -0300

    Add_class6_part4/5

--- Clase02/dar_formato_str.py
+++ Clase02/dar_formato_str.py
@@ -16,5 +16,12 @@ sueldo = 3000
 # mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:.2f}' .format(nombre, edad, sueldo)
 # print(mensaje_con_formato)
 
-mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+# mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+# print(mensaje) # La ventaja aqui es que podemos cambiar el orden, lo podemos manejar.
+
+mensaje = 'Nombre {n} Edad {e} Sueldo {s}'.format(n=nombre, e=edad, s=sueldo)
+# print(mensaje)
+
+diccionario = {'nombre': 'Ivan', 'Edad': 35, 'Sueldo': 8000.00}
+mensaje = 'Nombre {dic[nombre]} Edad {dic[Edad]} Sueldo {dic[Sueldo]:.2f}'.format(dic=diccionario)
 print(mensaje)
